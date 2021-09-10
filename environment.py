05572c56cb45b8400aeffe3b1b92e015c02de05b
amirreza sokhankhosh <amirrezasokhankhosh79@gmail.com>
2021-09-11 02:08:09 +0430

    Updating environment : completing environment features

--- environment.py
+++ environment.py
@@ -5,7 +5,7 @@
 
 from RLGlue import BaseEnvironment
 import numpy as np
-import gym
+from numpy.core.numeric import array_equal
 
 
 class TicTacToeEnvironment(BaseEnvironment):
@@ -13,8 +13,8 @@ class TicTacToeEnvironment(BaseEnvironment):
         """
         Setup for the environment called when the experiment first starts.
         """
-        self.env = gym.make("TicTacToe-v1")
-        self.env.seed(0)
+        # initialize board
+        self.board = np.zeros((3, 3))
 
     def env_start(self):
         """
@@ -26,7 +26,9 @@ class TicTacToeEnvironment(BaseEnvironment):
         """
 
         reward = 0.0
-        observation = self.env.reset()
+        state = self.board
+        actions = self.find_actions(state)
+        observation = (state, actions)
         is_terminal = False
 
         self.reward_obs_term = (reward, observation, is_terminal)
@@ -45,9 +47,67 @@ class TicTacToeEnvironment(BaseEnvironment):
                 and boolean indicating if it's terminal.
         """
 
-        last_state = self.reward_obs_term[1]
-        current_state, reward, is_terminal, _ = self.env.step(action)
+        self.take_action(action)
+        state = self.board
+        actions = self.find_actions(state)
+        observation = (state, actions)
+        reward, is_terminal = self.get_reward()
 
-        self.reward_obs_term = (reward, current_state, is_terminal)
+        self.reward_obs_term = (reward, observation, is_terminal)
 
         return self.reward_obs_term
+
+    def find_actions(state):
+        actions = []
+        for x in state:
+            for y in x:
+                if y == 0:
+                    actions.append((x, y))
+        return actions
+    
+    def get_board(self):
+        return self.board
+
+    def set_board(self, board):
+        self.board = board
+        
+    def take_action(self, action):
+        """
+            0 represents none chosen grid.
+            1 represents agent action.
+            2 represents opponent action.
+        """
+        self.board[action[0], action[1]] = 1
+        
+    def get_reward(self):
+        """
+            Win : 10
+            Draw : 0
+            Loose : -10
+            Each time step : -1
+        """
+        reward = -1
+        is_terminal = False
+        if np.count_nonzero(self.board) == 9:
+            is_terminal = True
+        for x in self.board:
+            if np.array_equal(x , np.ones(3)):
+                reward += 10
+                is_terminal = True
+            elif np.array_equal(x , np.full(3, 2)):
+                reward += -10
+                is_terminal = True
+        for x in self.board.T:
+            if np.array_equal(x , np.ones(3)):
+                reward += 10
+                is_terminal = True
+            elif np.array_equal(x , np.full(3, 2)):
+                reward += -10
+                is_terminal = True
+        if np.array_equal(np.diagonal(self.board), np.full(3, 1)) or np.array_equal(np.fliplr(self.board).diagonal(), np.full(3, 1)):
+            reward += 10
+            is_terminal = True
+        elif np.array_equal(np.diagonal(self.board), np.full(3, 2)) or np.array_equal(np.fliplr(self.board).diagonal(), np.full(3, 2)):
+            reward += -10
+            is_terminal = True
+        return reward, is_terminal
